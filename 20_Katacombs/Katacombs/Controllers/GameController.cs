9ef5e96a6328709140a655bf432ca93138430581
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-08-20 15:34:53 +0200

    feat: fix starting code

--- 20_Katacombs/Katacombs/Controllers/GameController.cs
+++ 20_Katacombs/Katacombs/Controllers/GameController.cs
@@ -23,48 +23,48 @@ namespace Katacombs.Controllers
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Help
-        /// </summary>
-        /// <remarks>Returns avaible commands and game hints for player</remarks>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <response code="200">Available commands and hints returned to caller</response>
-        [HttpGet]
-        [Route("/game/player/{playerSid}")]
-        [SwaggerOperation("GetHelpAndHints")]
-        [SwaggerResponse(statusCode: 200, type: typeof(HelpResponse), description: "Available commands and hints returned to caller")]
-        public virtual IActionResult GetHelpAndHints([FromRoute][Required] string playerSid)
-        {
-            throw new NotImplementedException();
-        }
+        // /// <summary>
+        // /// Help
+        // /// </summary>
+        // /// <remarks>Returns avaible commands and game hints for player</remarks>
+        // /// <param name="playerSid">Player unique identification</param>
+        // /// <response code="200">Available commands and hints returned to caller</response>
+        // [HttpGet]
+        // [Route("/game/player/{playerSid}")]
+        // [SwaggerOperation("GetHelpAndHints")]
+        // [SwaggerResponse(statusCode: 200, type: typeof(HelpResponse), description: "Available commands and hints returned to caller")]
+        // public virtual IActionResult GetHelpAndHints([FromRoute][Required] string playerSid)
+        // {
+        //     throw new NotImplementedException();
+        // }
 
-        /// <summary>
-        /// list of active players
-        /// </summary>
-        /// <remarks>Returns the list of active players</remarks>
-        /// <response code="200">list of players</response>
-        [HttpGet]
-        [Route("/game/player")]
-        [SwaggerOperation("ListPlayers")]
-        [SwaggerResponse(statusCode: 200, type: typeof(PlayersResponse), description: "list of players")]
-        public virtual IActionResult ListPlayers()
-        {
-            throw new NotImplementedException();
-        }
+        // /// <summary>
+        // /// list of active players
+        // /// </summary>
+        // /// <remarks>Returns the list of active players</remarks>
+        // /// <response code="200">list of players</response>
+        // [HttpGet]
+        // [Route("/game/player")]
+        // [SwaggerOperation("ListPlayers")]
+        // [SwaggerResponse(statusCode: 200, type: typeof(PlayersResponse), description: "list of players")]
+        // public virtual IActionResult ListPlayers()
+        // {
+        //     throw new NotImplementedException();
+        // }
 
-        /// <summary>
-        /// Quit Game
-        /// </summary>
-        /// <remarks>By removing the user she quits the game</remarks>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <response code="200">Game finshed</response>
-        /// <response code="404">Player with Sid playerSid not found</response>
-        [HttpDelete]
-        [Route("/game/player/{playerSid}")]
-        [SwaggerOperation("QuitGame")]
-        public virtual IActionResult QuitGame([FromRoute][Required] string playerSid)
-        {
-            throw new NotImplementedException();
-        }
+        // /// <summary>
+        // /// Quit Game
+        // /// </summary>
+        // /// <remarks>By removing the user she quits the game</remarks>
+        // /// <param name="playerSid">Player unique identification</param>
+        // /// <response code="200">Game finshed</response>
+        // /// <response code="404">Player with Sid playerSid not found</response>
+        // [HttpDelete]
+        // [Route("/game/player/{playerSid}")]
+        // [SwaggerOperation("QuitGame")]
+        // public virtual IActionResult QuitGame([FromRoute][Required] string playerSid)
+        // {
+        //     throw new NotImplementedException();
+        // }
     }
 }
--- 20_Katacombs/Katacombs/Controllers/PlayerController.cs
+++ 20_Katacombs/Katacombs/Controllers/PlayerController.cs
@@ -10,39 +10,6 @@ namespace Katacombs.Controllers
     public class PlayerController : ControllerBase
     {
         /// <summary>
-        /// Bag content
-        /// </summary>
-        /// <remarks>Returns the current list of items in player&#x27;s bag</remarks>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <response code="200">Bag content</response>
-        /// <response code="404">Player with Sid playerSid not found</response>
-        [HttpGet]
-        [Route("/player/{playerSid}/bag")]
-        [SwaggerOperation("GetBag")]
-        [SwaggerResponse(statusCode: 200, type: typeof(BagResponse), description: "Bag content")]
-        public virtual IActionResult GetBag([FromRoute][Required] string playerSid)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Look at specific direction
-        /// </summary>
-        /// <remarks>Returns information about a location in a specific direction</remarks>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <param name="direction"></param>
-        /// <response code="200">Location description for a direction</response>
-        /// <response code="404">Player with Sid playerSid not found</response>
-        [HttpGet]
-        [Route("/player/{playerSid}/look/{direction}")]
-        [SwaggerOperation("GetDirectionDescription")]
-        [SwaggerResponse(statusCode: 200, type: typeof(LocationResponse), description: "Location description for a direction")]
-        public virtual IActionResult GetDirectionDescription([FromRoute][Required] string playerSid, [FromRoute][Required] DirectionRequest direction)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
         /// Player location
         /// </summary>
         /// <remarks>Returns information about a location</remarks>
@@ -53,42 +20,75 @@ namespace Katacombs.Controllers
         [Route("/player/{playerSid}/location")]
         [SwaggerOperation("LookArround")]
         [SwaggerResponse(statusCode: 200, type: typeof(LocationResponse), description: "Room description")]
-        public virtual IActionResult LookArround([FromRoute][Required] string playerSid)
+        public virtual IActionResult LookAround([FromRoute][Required] string playerSid)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Move
-        /// </summary>
-        /// <remarks>Move in specified direction</remarks>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <param name="direction"></param>
-        /// <response code="200">Moved in requested direction</response>
-        /// <response code="405">Cannot move in requested direction</response>
-        [HttpPut]
-        [Route("/player/{playerSid}/move/{direction}")]
-        [SwaggerOperation("Move")]
-        public virtual IActionResult Move([FromRoute][Required] string playerSid, [FromRoute][Required] DirectionRequest direction)
-        {
-            throw new NotImplementedException();
-        }
+        //         /// <summary>
+        //         /// Bag content
+        //         /// </summary>
+        //         /// <remarks>Returns the current list of items in player&#x27;s bag</remarks>
+        //         /// <param name="playerSid">Player unique identification</param>
+        //         /// <response code="200">Bag content</response>
+        //         /// <response code="404">Player with Sid playerSid not found</response>
+        //         [HttpGet]
+        //         [Route("/player/{playerSid}/bag")]
+        //         [SwaggerOperation("GetBag")]
+        //         [SwaggerResponse(statusCode: 200, type: typeof(BagResponse), description: "Bag content")]
+        //         public virtual IActionResult GetBag([FromRoute][Required] string playerSid)
+        //         {
+        //             throw new NotImplementedException();
+        //         }
 
-        /// <summary>
-        /// Use an item
-        /// </summary>
-        /// <param name="playerSid">Player unique identification</param>
-        /// <param name="itemSid"></param>
-        /// <param name="action"></param>
-        /// <response code="200">Action performed</response>
-        /// <response code="404">Player or Item with Sid playerSid not found</response>
-        /// <response code="405">Action not available for specified item</response>
-        [HttpPut]
-        [Route("/player/{playerSid}/item/{itemSid}/use/{action}")]
-        [SwaggerOperation("UseItem")]
-        public virtual IActionResult UseItem([FromRoute][Required] string playerSid, [FromRoute][Required] string itemSid, [FromRoute][Required] Action action)
-        {
-            throw new NotImplementedException();
-        }
+        //         /// <summary>
+        //         /// Look at specific direction
+        //         /// </summary>
+        //         /// <remarks>Returns information about a location in a specific direction</remarks>
+        //         /// <param name="playerSid">Player unique identification</param>
+        //         /// <param name="direction"></param>
+        //         /// <response code="200">Location description for a direction</response>
+        //         /// <response code="404">Player with Sid playerSid not found</response>
+        //         [HttpGet]
+        //         [Route("/player/{playerSid}/look/{direction}")]
+        //         [SwaggerOperation("GetDirectionDescription")]
+        //         [SwaggerResponse(statusCode: 200, type: typeof(LocationResponse), description: "Location description for a direction")]
+        //         public virtual IActionResult GetDirectionDescription([FromRoute][Required] string playerSid, [FromRoute][Required] DirectionRequest direction)
+        //         {
+        //             throw new NotImplementedException();
+        //         }
+        //
+        //         /// <summary>
+        //         /// Move
+        //         /// </summary>
+        //         /// <remarks>Move in specified direction</remarks>
+        //         /// <param name="playerSid">Player unique identification</param>
+        //         /// <param name="direction"></param>
+        //         /// <response code="200">Moved in requested direction</response>
+        //         /// <response code="405">Cannot move in requested direction</response>
+        //         [HttpPut]
+        //         [Route("/player/{playerSid}/move/{direction}")]
+        //         [SwaggerOperation("Move")]
+        //         public virtual IActionResult Move([FromRoute][Required] string playerSid, [FromRoute][Required] DirectionRequest direction)
+        //         {
+        //             throw new NotImplementedException();
+        //         }
+
+        //         /// <summary>
+        //         /// Use an item
+        //         /// </summary>
+        //         /// <param name="playerSid">Player unique identification</param>
+        //         /// <param name="itemSid"></param>
+        //         /// <param name="action"></param>
+        //         /// <response code="200">Action performed</response>
+        //         /// <response code="404">Player or Item with Sid playerSid not found</response>
+        //         /// <response code="405">Action not available for specified item</response>
+        //         [HttpPut]
+        //         [Route("/player/{playerSid}/item/{itemSid}/use/{action}")]
+        //         [SwaggerOperation("UseItem")]
+        //         public virtual IActionResult UseItem([FromRoute][Required] string playerSid, [FromRoute][Required] string itemSid, [FromRoute][Required] Action action)
+        //         {
+        //             throw new NotImplementedException();
+        //         }
     }
 }
